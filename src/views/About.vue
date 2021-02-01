f8dc0bda221d80d43616b6283e5ee29db8702d72
admin <darshilshah7551@gmail.com>
2021-02-01 17:33:31 +0530

    added vuetify

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <v-card>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa deserunt
+        at numquam voluptatum ut atque, veritatis provident, dolores minus
+        placeat hic molestias similique veniam fugiat voluptates maiores? Iusto,
+        soluta ducimus?
+      </h1>
+    </v-card>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque magni
+      harum, et, laborum voluptates optio fugiat beatae accusamus molestias
+      omnis iure tenetur consectetur nisi saepe! Suscipit iusto expedita
+      numquam, eligendi iure molestiae dolore repellendus deserunt rem modi
+      molestias exercitationem voluptas praesentium sequi, ex eaque! Eligendi
+      voluptas qui perspiciatis! Officia nam, laudantium vero repudiandae
+      aliquam, veritatis perferendis illo ratione, ex excepturi atque incidunt
+      totam obcaecati maxime. Nihil et sint repellendus eum voluptas eveniet
+      saepe? Temporibus maxime vitae beatae fugit, ipsum deserunt impedit.
+      Atque, illo quod ducimus laborum provident aliquid nemo autem animi
+      cupiditate eveniet nihil voluptate perferendis unde possimus quae
+      explicabo?
+    </p>
   </div>
 </template>
